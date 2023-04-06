59e21c2af085ea504699375bfa2d2fba5854ec6a
@thiagofcasa <@thiagofcasa>
2023-04-06 20:57:21 +0100

    ConfigIdentityServerAndLoginScreen

---
+++ S10_DesenvFrontHome/GeekShopping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO prodVo);
+        Task<ProductVO> Update(ProductVO prodVo);
+        Task<bool> Delete(long id);
+    }
+}
