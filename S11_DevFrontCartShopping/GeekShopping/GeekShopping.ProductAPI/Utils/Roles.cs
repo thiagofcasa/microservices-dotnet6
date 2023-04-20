12ea4b872f174ce1a758cc190b521d9f75866955
@thiagofcasa <@thiagofcasa>
2023-04-20 11:26:42 +0100

    GeekShoppingWithCart

---
+++ S11_DevFrontCartShopping/GeekShopping/GeekShopping.ProductAPI/Utils/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace GeekShopping.ProductAPI.Utils
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Client = "Client";
+    }
+}
